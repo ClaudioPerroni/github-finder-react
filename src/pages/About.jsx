b4695d62b715252e55fbacb0e62f733f27c259f0
claudio perroni <claudio.per@live.it>
2022-03-24 14:26:01 +0100

    primo commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+
+      <p>
+        :)
+      </p>
+    </div>
+  )
+}
+
+export default About
